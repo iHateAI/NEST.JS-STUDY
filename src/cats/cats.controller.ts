9f22d318d2837b70165deefdc9c292866e872136
HyunSeong <hhs950120@gmail.com>
2022-11-16 23:52:58 +0900

    미들웨어 만들기

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch, Delete } from '@nestjs/common';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete service';
+  }
+}
