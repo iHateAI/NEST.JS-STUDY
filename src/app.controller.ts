458d5ea3e24416cd35d190f28500786ae71108ee
HyunSeong <hhs950120@gmail.com>
2022-11-16 23:23:23 +0900

    다시 초기화

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,12 @@
-import { Body, Controller, Get, Param, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller } from '@nestjs/common';
+
 import { AppService } from './app.service';
 
-@Controller('cats')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello/:id')
-  getHello(
-    @Req() req: Request,
-    @Body() body,
-    @Param() param: { id: string; name: string },
-  ): string {
-    console.log(param);
-    //console.log(body);
-
-    // 책임 분리를 위해 param이나 body를 서비스에 넘기기
-    //return this.appService.getHello(body, param);
+  getHello() {
     return this.appService.getHello();
   }
 }
